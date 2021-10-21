eae7bfc21a87a92694c6a7ea362b017fd50e51e8
mubangizi <mubangizia22@gmail.com>
2021-10-21 14:41:52 +0300

    Initial commit

---
+++ returnexample/server/ServerImpl.java
@@ -0,0 +1,18 @@
+package returnexample.server;
+
+import returnexample.shared.UpperCaseServer;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ServerImpl implements UpperCaseServer {
+
+    public ServerImpl() throws RemoteException {
+        UnicastRemoteObject.exportObject(this, 0);
+    }
+
+    @Override
+    public String toUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}
